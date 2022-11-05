1675e6660f38d1413054e61b70fcf09352854925
Tim Hosey <tim.hosey@me.com>
2022-11-04 20:28:14 -0700

    adjusted to add 2x multiplier

--- src/index.ts
+++ src/index.ts
@@ -383,6 +383,12 @@ const buttons = (coreType: CoreType, id: string, multiplier: number = 1, enabled
         .setDisabled(!enabled)
         .setStyle(highlight == 'start' ? ButtonStyle.Success : ButtonStyle.Secondary);
 
+    const multiply2 = new ButtonBuilder()
+        .setCustomId(id + '-' + '2' + '-' + multiplier)
+        .setEmoji('3️2️⃣')
+        .setDisabled(!enabled)
+        .setStyle(highlight == '2' ? ButtonStyle.Success : ButtonStyle.Secondary);
+
     const multiply3 = new ButtonBuilder()
         .setCustomId(id + '-' + '3' + '-' + multiplier)
         .setEmoji('3️⃣')
@@ -414,7 +420,7 @@ const buttons = (coreType: CoreType, id: string, multiplier: number = 1, enabled
                     ),
                 new ActionRowBuilder()
                     .addComponents(
-                        multiply3, multiply5, multiply10
+                        multiply2, multiply3, multiply5, multiply10
                     )
             ] as any[];
 
@@ -434,7 +440,7 @@ const buttons = (coreType: CoreType, id: string, multiplier: number = 1, enabled
                     ),
                 new ActionRowBuilder()
                     .addComponents(
-                        multiply3, multiply5, multiply10
+                        multiply2, multiply3, multiply5, multiply10
                     )
             ] as any[];
 
@@ -450,7 +456,7 @@ const buttons = (coreType: CoreType, id: string, multiplier: number = 1, enabled
                     ),
                 new ActionRowBuilder()
                     .addComponents(
-                        multiply3, multiply5, multiply10
+                        multiply2, multiply3, multiply5, multiply10
                     )
             ] as any[];
 
@@ -470,7 +476,7 @@ const buttons = (coreType: CoreType, id: string, multiplier: number = 1, enabled
                     ),
                 new ActionRowBuilder()
                     .addComponents(
-                        multiply3, multiply5, multiply10
+                        multiply2, multiply3, multiply5, multiply10
                     )
             ] as any[];
     }
